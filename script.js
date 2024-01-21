c3c207124faa57933b3f317157267f72fec4a402
Vinay Kambar <vnkambar_b21@ce.vjti.ac.in>
2024-01-21 19:31:34 +0530

    added topics

--- script.js
+++ script.js
@@ -13,53 +13,65 @@ const repos = document.querySelector(".repos");
 button.addEventListener('click', () => {
     const value = input.value;
     const apiurl = `https://api.github.com/users/${value}`;
+
+    // Show loader for 3 seconds
     document.getElementById("loader").style.display = 'block';
     setTimeout(function(){
-      document.getElementById("loader").style.display = 'none';
-    
+        document.getElementById("loader").style.display = 'none';
 
-    fetch(apiurl)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            const fetchedUsername = data.login;
-            username.innerHTML=fetchedUsername;
-
-            const bio=data.bio;
-           if(bio!=null) desc.innerHTML=bio;
-
-            const loca=data.location;
-           if(loca!==null) loc.innerHTML=`<i class="fa-solid fa-location-dot"></i> ${loca}`;
-
-            const twit=data.twitter_username;
-           if(twit!==null) twitter.innerHTML=twit;
-
-            const img=data.avatar_url;
-           image.src=img;
-
-            const giturl=data.html_url;
-           githublink.innerHTML=`<i class="fa-solid fa-link"></i> ${giturl}`;
-
-            const repourl=data.repos_url;
-            fetch(repourl)
-            .then((data)=>data.json())
-            .then((data)=>{
+        fetch(apiurl)
+            .then((res) => res.json())
+            .then((data) => {
                 console.log(data);
-                let a=""
-                data.map((value)=>{
-                   a+=`<div class="a">
-                    <div class="name">${value.name}</div>
-                    <div class="repodesc">${value.description===null?'-':value.description}</div>
-                    <div class="lang">${value.language}</div>
-                   </div>`
-                })
-                repos.innerHTML=a;
-            })
+                const fetchedUsername = data.login;
+                username.innerHTML = fetchedUsername;
 
-        })
-        
-        .catch((error) => {
-            console.error("Error fetching data:", error);
-        });
-    }, 2000);
+                const bio = data.bio;
+                if (bio != null) desc.innerHTML = bio;
+
+                const loca = data.location;
+                if (loca !== null) loc.innerHTML = `<i class="fa-solid fa-location-dot"></i> ${loca}`;
+
+                const twit = data.twitter_username;
+                if (twit !== null) twitter.innerHTML = twit;
+
+                const img = data.avatar_url;
+                image.src = img;
+
+                const giturl = data.html_url;
+                githublink.innerHTML = `<i class="fa-solid fa-link"></i> ${giturl}`;
+
+                const repourl = data.repos_url;
+                fetch(repourl)
+                    .then((data) => data.json())
+                    .then((data) => {
+                        console.log(data);
+                        let a = "";
+                        data.map((value) => {
+                            let topicsHTML = ""; // Create an empty string to store HTML for topics
+                            if (value.topics && value.topics.length > 0) {
+                                // Check if 'topics' is an array and has items
+                                value.topics.forEach((topic) => {
+                                    topicsHTML += `<span class="topic">${topic}  </span>`;
+                                });
+                            }
+                            console.log(topicsHTML)
+                            a += `<div class="a">
+                                    <div class="name">${value.name}</div>
+                                    <div class="repodesc">${value.description===null?"<i>No Description</i>":value.description}</div>
+                                    <div class="lang">${topicsHTML}</div>
+                                </div>`;
+                        });
+                        repos.innerHTML = a;
+                    })
+                    .catch((error) => {
+                        console.error("Error fetching repo data:", error);
+                    });
+
+            })
+            .catch((error) => {
+                console.error("Error fetching user data:", error);
+            });
+    }, 3000); 
 });
+

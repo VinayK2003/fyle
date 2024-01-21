907c5c97b1b0639eef6740b76b2db10c17431128
Vinay Kambar <vnkambar_b21@ce.vjti.ac.in>
2024-01-21 20:14:00 +0530

    a

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ const twitter = document.querySelector(".twitter");
 const image = document.querySelector(".image");
 const githublink = document.querySelector(".githublink");
 const repos = document.querySelector(".repos");
+const paginationContainer = document.querySelector(".pagination");
+
+let currentPage = 1;
+const reposPerPage = 5;
 
 button.addEventListener('click', () => {
     const value = input.value;
@@ -42,27 +46,27 @@ button.addEventListener('click', () => {
                 githublink.innerHTML = `<i class="fa-solid fa-link"></i> ${giturl}`;
 
                 const repourl = data.repos_url;
-                fetch(repourl)
+                fetch(`${repourl}?page=1&per_page=${reposPerPage}`)
                     .then((data) => data.json())
                     .then((data) => {
                         console.log(data);
                         let a = "";
                         data.map((value) => {
-                            let topicsHTML = ""; // Create an empty string to store HTML for topics
+                            let topicsHTML = "";
                             if (value.topics && value.topics.length > 0) {
-                                // Check if 'topics' is an array and has items
                                 value.topics.forEach((topic) => {
                                     topicsHTML += `<span class="topic">${topic}  </span>`;
                                 });
                             }
-                            console.log(topicsHTML)
                             a += `<div class="a">
                                     <div class="name">${value.name}</div>
-                                    <div class="repodesc">${value.description===null?"<i>No Description</i>":value.description}</div>
+                                    <div class="repodesc">${value.description === null ? "<i>No Description</i>" : value.description}</div>
                                     <div class="lang">${topicsHTML}</div>
                                 </div>`;
                         });
                         repos.innerHTML = a;
+
+                      
                     })
                     .catch((error) => {
                         console.error("Error fetching repo data:", error);
@@ -72,6 +76,7 @@ button.addEventListener('click', () => {
             .catch((error) => {
                 console.error("Error fetching user data:", error);
             });
-    }, 3000); 
+    }, 3000);
 });
+
 
